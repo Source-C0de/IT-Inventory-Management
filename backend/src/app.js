0ae64902de5575d20192634ea9243b46521deb22
Fahmim Mohammod Shahriar <63873694+Source-C0de@users.noreply.github.com>
2025-03-11 00:17:02 +0300

    added boiler code for connecting local mysql database with project

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const app = express();
 const PORT =  process.env.PORT || 3000;
 
+const connectDB = require('./config/mysqlDb');
+
+
+connectDB.DbConnection();
 
 
 app.get('/', (req,res) => {
     res.send("hello world");
-})
-
-app.listen(PORT, () => {
-    console.log("Server is running in port ${PORT}");
-
 });
+
+
+module.exports = app;
---
+++ backend/src/config/mysqlDb.js
@@ -0,0 +1,41 @@
+
+const mysql = require('mysql2');
+
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT,
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0
+//   });
+
+async function DbConnection() {
+
+    try{
+        const connection = mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            port: process.env.DB_PORT,
+            
+        });
+        console.log("MySQL DB successfully connected");
+        return connection;
+    }catch(err){
+        console.error('Error connecting to MySQL Database:', err);
+    }
+    
+}
+
+module.exports = {
+    DbConnection
+}
